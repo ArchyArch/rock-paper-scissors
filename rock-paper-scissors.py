d9e5e0abf405f86770932cba08f78dcd0b94a6ec
Magdalena Nowowiejska <nowowiejska.magda@gmail.com>
2019-10-27 22:05:13 +0100

    Ask player if he wants to play again
    TODO: Ask if he wants to keep going until proper choice is made

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,30 +1,40 @@
 from random import randint
+from sys import exit
 
 choose = ["Rock", "Paper", "Scissors"]
 computer = choose[randint(0,2)]
 player = False
+answer = "maybe"
 
 while player == False:
-    player = input("Rock, Paper, Scissors?")
-    if player == computer:
-        print("Tie!")
-    elif player == "Rock" or "rock":
-        if computer == "Paper" or "paper":
-            print("You lose!", computer, "covers", player)
+    while answer != "yes" or answer != "no":
+        player = input("Rock, Paper, Scissors?")
+        if player.lower() == computer.lower():
+            print("Tie!")
+        elif player.lower() == "rock":
+            if computer == "Paper":
+                print("You lose!", computer, "covers", player)
+            else:
+                print("You win!", player, "smashes", computer)
+        elif player.lower() == "paper":
+            if computer == "Scissors":
+                print("You lose!", computer, "cut", player)
+            else:
+                print("You win!", player, "covers", computer)
+        elif player.lower() == "scissors":
+            if computer == "Rock":
+                print("You lose...", computer, "smashes", player)
+            else:
+                print("You win!", player, "cut", computer)
         else:
-            print("You win!", player, "smashes", computer)
-    elif player == "Paper" or "paper":
-        if computer == "Scissors" or "scissors":
-            print("You lose!", computer, "cut", player)
-        else:
-            print("You win!", player, "covers", computer)
-    elif player == "Scissors" or "scissors":
-        if computer == "Rock" or "rock":
-            print("You lose...", computer, "smashes", player)
-        else:
-            print("You win!", player, "cut", computer)
-    else:
-        print("Try again. Choose a valid option!")
+            print("Try again. Choose a valid option!")
 
-    player = False
-    computer = choose[randint(0,2)]
+        answer = input("Do you want to play again? Yes or no ")
+
+        if answer.lower() == "yes":
+            player = False
+            computer = choose[randint(0,2)]
+        elif answer.lower() == "no":
+            exit()
+        else:
+            answer = input("Try again. Choose a valid option! ")
